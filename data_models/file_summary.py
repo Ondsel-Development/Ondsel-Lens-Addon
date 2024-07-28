98cd1e4a193f00184bd8d0a627b4761bf8912e39
John Dupuy <jdupuy98@gmail.com>
2024-07-27 21:20:52 -0500

    added missing FileSummary variant

--- data_models/file_summary.py
+++ data_models/file_summary.py
@@ -9,3 +9,13 @@ class FileSummary:
     modelId: str
     currentVersion: FileVersion
     thumbnailUrlCache: str = None
+
+
+# the "Limited" variant is to prevent recursive references and simplify data structures
+@dataclass(frozen=True, order=True)
+class FileSummary_CurationLimited:
+    _id: str
+    custFileName: str
+    modelId: str
+    # currentVersion: FileVersion   # <-- we avoid deserializing all of this
+    thumbnailUrlCache: str = None
