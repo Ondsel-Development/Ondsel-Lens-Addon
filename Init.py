d317ce3cf3aaff6f6abbf925c9a9ca469fd019a4
Pieter Hijma <pieterhijma@users.noreply.github.com>
2024-08-01 10:32:24 +0200

    Support Mac OS for ondsel-lens URLs
    Note that the Info.plist file in the Mac OS bundles needs to register
    the ondsel-lens URL scheme as well.

--- Init.py
+++ Init.py
@@ -1,9 +1,6 @@
 import FreeCAD
-import register_lens_handler
 
 # param_group = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Start")
 # param_group.SetBool("ShowOnStartup", False)
 
 FreeCAD.Console.PrintMessage("Init Lens\n")
-
-register_lens_handler.register_lens_handler()
--- InitGui.py
+++ InitGui.py
@@ -8,6 +8,7 @@ import PySide.QtCore as QtCore
 import FreeCADGui as Gui
 
 from lens_command import LensCommand, LensWorkbenchManipulator, start_mdi_tab
+import register_lens_handler
 
 import WorkspaceView
 
@@ -18,3 +19,4 @@ Gui.addWorkbenchManipulator(LensWorkbenchManipulator())
 start_mdi_tab()
 
 QtCore.QTimer.singleShot(3000, WorkspaceView.runsAfterLaunch)
+register_lens_handler.register_lens_handler()
