0afbed293bccc6553262a282d3915df78339c362
Pieter Hijma <pieterhijma@users.noreply.github.com>
2024-10-17 20:33:01 +0200

    Add wait cursors for global MDI view
    Public shares are also not loaded on initialization anymore to make the
    startup time shorter.

--- Init.py
+++ Init.py
@@ -1,6 +1,2 @@
-import FreeCAD
-
 # param_group = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Start")
 # param_group.SetBool("ShowOnStartup", False)
-
-FreeCAD.Console.PrintMessage("Init Lens\n")
