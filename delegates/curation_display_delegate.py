fa1cda486624a1ee485a19db3366992fd1e482ff
Pieter Hijma <pieterhijma@users.noreply.github.com>
2024-10-18 11:07:25 +0200

    Adopt wait cursor for Ondsel Start

--- delegates/curation_display_delegate.py
+++ delegates/curation_display_delegate.py
@@ -1,5 +1,3 @@
-import copy
-
 import requests
 import webbrowser
 
@@ -12,11 +10,15 @@ from components.choose_download_action_dialog import ChooseDownloadActionDialog
 from components.choose_from_lens_dialog import ChooseFromLensDialog
 from components.choose_workspace_action_dialog import ChooseWorkspaceActionDialog
 
+from Utils import wait_cursor
+
 logger = Utils.getLogger(__name__)
 
 
 class CurationDisplayDelegate(QFrame):
-    """delegate for curation display in general; should be used by children via inheritance not directly"""
+    """delegate for curation display in general; should be used by children via
+    inheritance not directly
+    """
 
     def __init__(self, index=None):
         super().__init__()
@@ -28,7 +30,8 @@ class CurationDisplayDelegate(QFrame):
         self._preload_icon()
         image_url = (
             self.curation.get_thumbnail_url()
-        )  # sadly, this cannot be queued because API does not work in thread for some reason
+        )  # sadly, this cannot be queued because API does not work in thread
+        # for some reason
         self.thread = QThread()
         self.worker = _GetCurationImage()
         self.worker.image_url = image_url
@@ -49,26 +52,30 @@ class CurationDisplayDelegate(QFrame):
 
     def _take_action(self):
         if self.curation.collection == "shared-models":
-            data_parent = self.curation.parent
-            dlg = ChooseDownloadActionDialog(self.curation.name, data_parent)
+            with wait_cursor():
+                data_parent = self.curation.parent
+                dlg = ChooseDownloadActionDialog(self.curation.name, data_parent)
             overall_response = dlg.exec()
             if overall_response != 0:
                 if dlg.answer == ChooseDownloadActionDialog.OPEN_ON_WEB:
                     self._goto_url()
                 elif dlg.answer == ChooseDownloadActionDialog.DL_TO_MEM:
-                    msg = handlers.download_shared_model_to_memory(
-                        self.curation.parent.api, str(self.curation._id)
-                    )
+                    with wait_cursor():
+                        msg = handlers.download_shared_model_to_memory(
+                            self.curation.parent.api, str(self.curation._id)
+                        )
                     if msg is False:
                         logger.warn("Unable to download; opening in browser instead.")
                         self._goto_url()
                     else:
                         logger.warn(
-                            f"{msg}. Be sure to save to disk if you want to keep the model."
+                            f"{msg}. Be sure to save to disk if you want"
+                            "to keep the model."
                         )
         elif self.curation.collection == "workspaces":
-            data_parent = self.curation.parent
-            dlg = ChooseWorkspaceActionDialog(self.curation.name, data_parent)
+            with wait_cursor():
+                data_parent = self.curation.parent
+                dlg = ChooseWorkspaceActionDialog(self.curation.name, data_parent)
             overall_response = dlg.exec()
             if overall_response != 0:
                 if dlg.answer == ChooseWorkspaceActionDialog.OPEN_ON_WEB:
@@ -93,12 +100,13 @@ class CurationDisplayDelegate(QFrame):
         if overall_response == 0:
             return
         file_detail = dlg.answer["file"]
-        msg = handlers.download_file_version_to_memory(
-            self.curation.parent.api,
-            file_detail._id,
-            file_detail.currentVersion._id,
-            True,
-        )
+        with wait_cursor():
+            msg = handlers.download_file_version_to_memory(
+                self.curation.parent.api,
+                file_detail._id,
+                file_detail.currentVersion._id,
+                True,
+            )
         if msg is False:
             logger.warn("Unable to download; opening in browser instead.")
             self._goto_url()
@@ -132,14 +140,18 @@ def get_pixmap_from_url(thumbnailUrl):
 
 class _GetCurationImage(QObject):
     """
-    This thread worker class is strictly for use by the single thread started in CurationDisplayDelegate (and it's children).
-    The child will call the `start_image_load()` method of CurationDisplayDelegate during initialization. That will,
-    in turn, start this thread. This thread then emits the result when image is in memory.
+    This thread worker class is strictly for use by the single thread started
+    in CurationDisplayDelegate (and it's children).  The child will call the
+    `start_image_load()` method of CurationDisplayDelegate during
+    initialization. That will, in turn, start this thread. This thread then
+    emits the result when image is in memory.
+
     """
 
     finished = Signal(
         QPixmap, bool
-    )  # the bool is needed because "None" is still re-interpreted by the C lib as QPixMap
+    )  # the bool is needed because "None" is still re-interpreted by the C lib
+    #  as QPixMap
 
     def __init__(self):
         super().__init__()
